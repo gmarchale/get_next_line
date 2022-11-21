eeafcf0732950a0d252776305e5e463c57c4caab
Gaetan Marchale <gmarchal@fu-r1-p5.s19.be>
2022-11-21 16:57:40 +0100

    read 1st line

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,88 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 11:17:25 by gmarchal          #+#    #+#             */
-/*   Updated: 2022/11/18 18:36:40 by gmarchal         ###   ########.fr       */
+/*   Updated: 2022/11/21 16:56:52 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>		// enlever cette ligne
+#include <stdlib.h>
+#include <fcntl.h>		// delete (pour open)
+#include <stdio.h>		// delete
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ret;
+
+	if (!s1 || !s2)
+		return (0);
+	ret = malloc (sizeof(char const) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!ret)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		ret[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		ret[j++] = s2[i];
+		i++;
+	}
+	ret[j] = '\0';
+	//rajouter un free
+	return (ret);
+}
 
 char *get_next_line(int fd)
 {
-	static char variable[BUFFER_SIZE + 1];
+	//static char my_static[BUFFER_SIZE + 1];
 	char my_buffer[BUFFER_SIZE + 1];
-	int	i;
+	char *my_line;
+	int i;
 	int j;
+	int end;
 
-	i = 0;
+	end = 0;
 	j = 0;
-	read(fd, my_buffer, BUFFER_SIZE);
-	my_buffer[BUFFER_SIZE] = '\0';
-
-	//printf("%s\n", my_buffer);
-	while(my_buffer[i] || my_buffer[i] != '\n')
+	my_line = malloc (sizeof(char) * (BUFFER_SIZE + 1)); //pas certain que ca soit la bonne taille de malloc
+	while (end != 1)
 	{
-		if (my_buffer[i] == '\n')
+		read(fd, my_buffer, BUFFER_SIZE);
+		i = 0;
+		while (my_buffer[i] && end != 1)
 		{
-			break ;
+			if (my_buffer[i] == '\n')
+				end = 1;
+			else
+			{
+				my_line[j] = my_buffer[i];
+				j++;
+				i++;
+			}
 		}
-		else
-			write(1, &my_buffer[i], 1);
-		i++;
 	}
-	while (my_buffer[i])
-	{
-		variable[j] = my_buffer[i + j];
-		j++;
-	}
-
-	return (0);
+	return (my_line);
 }
-
 int main(void)
 {
 	int	fd;
 
 	fd = open("text1.txt", O_RDONLY);
-	get_next_line(fd);
+	printf("%s\n", get_next_line(fd));
 }
